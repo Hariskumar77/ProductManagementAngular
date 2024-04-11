7d8fb67c6b375354d4126927e37003bcdea18133
ZEI1COB <Senniappan.Hariskumar@in.bosch.com>
2024-04-11 20:08:52 +0530

    FinalFrontEnd-ProductManagement

--- src/app/product-add-component/product-add-component.component.ts
+++ src/app/product-add-component/product-add-component.component.ts
@@ -1,124 +1,8 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Product } from '../product.model';
-// import { ProductService } from '../product.service';
-
-// @Component({
-//   selector: 'app-product-add-component',
-//   templateUrl: './product-add-component.component.html',
-//   styleUrls: ['./product-add-component.component.css']
-// })
-// export class ProductAddComponentComponent implements OnInit {
-
-//   productForm: FormGroup;
-//   product:Product[];
-//   constructor(private fb: FormBuilder, private productService: ProductService) { }
-
-//   ngOnInit(): void {
-//     this.productForm = this.fb.group({
-//       productName: ['', Validators.required],
-//       description: ['', Validators.required],
-//       price: ['', Validators.required],
-//       manufacturedDate: ['', Validators.required]
-//     });
-//   }
-
-//   onSubmit() {
-
-//     if (this.productForm.valid) {
-//       const product = {
-//         // productID:99,
-//         productName: this.productForm.get('productName').value,
-//         description: this.productForm.get('description').value,
-//         price: this.productForm.get('price').value,
-//         manufacturedDate: this.productForm.get('manufacturedDate').value
-//       };
-
-//       console.log(product);
-
-//       this.productService.postProduct(product).subscribe(
-//         (res: any) => {
-//           console.log("New Product Created");
-//           alert("Product Added");
-//           this.productForm.reset(); // Reset the form after successful submission
-//         },
-//         (error: any) => {
-//           console.error("Error:", error);
-//           alert("Failed to add product. Please try again.");
-//         }
-//       );
-//     }
-//   }
-// }
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Product } from '../product.model';
-// import { ProductService } from '../product.service';
-// import { MatDialogRef } from '@angular/material/dialog';
-
-
-
-// @Component({
-//   selector: 'app-product-add-component',
-//   templateUrl: './product-add-component.component.html',
-//   styleUrls: ['./product-add-component.component.css']
-// })
-// export class ProductAddComponentComponent implements OnInit {
-
-//   productForm: FormGroup;
-//   product: Product[];
-  
-
-//   constructor(private fb: FormBuilder,
-//      private productService: ProductService,
-//      private dialogRef: MatDialogRef<ProductAddComponentComponent> // Inject MatDialogRef
-// ) { }
-
-//   ngOnInit(): void {
-//     this.productForm = this.fb.group({
-//       productName: ['', Validators.required],
-//       description: ['', Validators.required],
-//       price: ['', [Validators.required, priceValidator]], 
-//       manufacturedDate: ['', Validators.required]
-//     });
-//   }
-
-//   onSubmit() {
-//     if (this.productForm.valid) {
-//       const product = {
-//         productName: this.productForm.get('productName').value,
-//         description: this.productForm.get('description').value,
-//         price: this.productForm.get('price').value,
-//         manufacturedDate: this.productForm.get('manufacturedDate').value
-//       };
-
-//       this.productService.postProduct(product).subscribe(
-//         (res: any) => {
-//           console.log("New Product Created");
-//           //alert("Product Added");
-//           this.productService.announceProductAdded(); // Announce that a product has been added
-//           this.productForm.reset(); // Reset the form after successful submission
-//           this.dialogRef.close(); // Close the dialog
-
-//         },
-//         (error: any) => {
-//           console.error("Error:", error);
-//           alert("Failed to add product. Please try again.");
-//         }
-//       );
-//     }
-//   }
-// }
-
-
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 import { MatDialogRef } from '@angular/material/dialog';
-import { trigger, transition, style, animate } from '@angular/animations';
-
 
 function priceValidator(control) {
   const price = parseFloat(control.value);
@@ -132,18 +16,6 @@ function priceValidator(control) {
   selector: 'app-product-add-component',
   templateUrl: './product-add-component.component.html',
   styleUrls: ['./product-add-component.component.css'],
-
-  animations: [
-    trigger('fadeInOut', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('300ms ease-out', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('300ms ease-in', style({ opacity: 0 })),
-      ]),
-    ]),
-  ],
 })
 export class ProductAddComponentComponent implements OnInit {
   productForm: FormGroup;
@@ -159,7 +31,6 @@ export class ProductAddComponentComponent implements OnInit {
     this.productForm = this.fb.group({
       productName: ['', Validators.required],
       description: ['', Validators.required],
-      // price: ['', [Validators.required]],
       price: ['', [Validators.required, priceValidator]], 
       manufacturedDate: ['', Validators.required]
     });
--- src/app/product-details-component/product-details-component.component.ts
+++ src/app/product-details-component/product-details-component.component.ts
@@ -1,35 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Product } from '../product.model';
-// import { ProductService } from '../product.service';
-// import { ActivatedRoute } from '@angular/router';
-
-// @Component({
-//   selector: 'app-product-details-component',
-//   templateUrl: './product-details-component.component.html',
-//   styleUrls: ['./product-details-component.component.css']
-// })
-// export class ProductDetailsComponentComponent implements OnInit {
-//   product: Product;
-//   productId: number;
-
-//   constructor(private productService: ProductService, private route: ActivatedRoute) { }
-
-//   ngOnInit() {
-//     this.route.params.subscribe(params => {
-//       console.log("Product ID1:",params['id'] );
-
-//       this.productId = +params['id']; // Convert the parameter to a number
-//       console.log("Product ID:", this.productId);
-//       this.productService.getProductById(this.productId).subscribe(
-//         data => {
-//           this.product = data;
-//           console.log(this.product);
-//         }
-//       );
-//     });
-//   }
-// }
-
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Product } from '../product.model';
